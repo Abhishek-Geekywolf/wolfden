70d5cb2761e55955add321da809814cfd4a914ba
abhishek-geekywolf <abhishek.sadasivan@geekywolf.com>
2024-11-15 15:56:24 +0530

    My team and comments done

--- src/services/WolfDen.Application/DTOs/Employees/EmployeeHierarchyDto.cs
+++ src/services/WolfDen.Application/DTOs/Employees/EmployeeHierarchyDto.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int EmployeeCode { get; set; }
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
@@ -12,6 +12,7 @@
         public int? DesignationId { get; set; }
         public int? DepartmentId { get; set; }
         public int? ManagerId { get; set; }
+        public bool? IsActive { get; set; }
         public List<EmployeeHierarchyDto> Subordinates { get; set; }
 
 
